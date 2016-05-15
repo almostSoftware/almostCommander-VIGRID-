6579c324414f25ba918c1a7917df175c81ff876d
almostSoftware <s_nowosad@o2.pl>
2016-05-15 22:11:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,11 +23,10 @@ int main()
         cout<<"[2] Informacje o systemie"           <<endl;
         cout<<"[3] Zarządzanie Kernelami"           <<endl;
         cout<<"[4] Instalator środowisk graficzmych"<<endl;
-        cout<<"[5] Inne"                            <<endl<<endl;
-
+        cout<<"[5] Inne"                            <<endl;
         cout<<"[6] Uruchom / Wykonaj"               <<endl<<endl;
 
-        cout<<"[7] O 'Vigrid'"                      <<endl<<endl;
+        //cout<<"[7] O 'Vigrid'"                    <<endl<<endl;
 
         cout<<"[q] Zakończ program"                 <<endl;
 
@@ -70,26 +69,10 @@ int main()
                 break;
             }
 
-            case '7':	// O 'VIGRID'
+            /*case '7':	// O 'VIGRID'
             {
-
-                cout<<"almostCommander 'VIGRID' to prosty programik konsolowy napisany w C++."
-                       "\nJego głównym celem jest ułatwienie i przyspieszenie podstawowych operacji w terminalu."
-                       "\nDodatkowo program został zaopatrzony m.in. w możliwość dobierania multimediów. "
-
-                       "\n\nWszystkie dostępne opcje 'VIGRID' zostały wstępnie przetestowane i nie powinny stwarzać problemów, jednak..."
-
-                       "\nJest to wersja testowa, proszę zachować szczególną ostrożność, najbezpieczniej będzie korzystać z programu"
-                       "\nza pośrednictwem dystrybucji zainstalowanej na Virtualboxie lub LiveCD. "<<endl<<endl;
-
-
-                cout<<"Powrót do MENU [enter]";
-
-                getchar();getchar();
-                system("clear");
-
-                break;
-            }
+            
+            }*/
                 case 'q':
                 exit(0);
         }
